c26b7b41ae022c1cfab30a943bd6a10719d850a9
dmtr <dmmytrenko@gmail.com>
2025-04-15 11:34:37 +0300

    basic infra added

---
+++ src/WarehouseOptimizer.Contracts/Commands/RegisterSKUCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace WarehouseOptimizer.Contracts.Commands;
+
+public class RegisterSKUCommand
+{
+    public string SkuCode { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Height { get; set; }
+    public decimal Width { get; set; }
+    public decimal Length { get; set; }
+}
---
+++ src/WarehouseOptimizer.Worker/Program.cs
@@ -0,0 +1,7 @@
+using WarehouseOptimizer.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
